1a477a4c993e76ccf6bec074bcd703748534bec9
Danylo Bilyi <danilo.bilyi@gmail.com>
2023-07-16 02:33:13 +0300

    init and setup project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  sassOptions: {
+    prependData: `@use 'src/styles/variables' as *;`,
+  },
+};
+
+module.exports = nextConfig;
