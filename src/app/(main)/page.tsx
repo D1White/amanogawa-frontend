b640c53ea360afefad304197866fda91e23501d1
Danylo Bilyi <danilo.bilyi@gmail.com>
2023-10-24 05:21:12 +0300

    feat: add test logs

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,14 @@
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+import { cookies } from 'next/headers';
+
 import { NewAnime, TrendingAnime, YoutubeVideos } from '@/features';
 
 export default async function Home() {
+  const cookieStore = cookies();
+  console.log('[INFO] all cookies', cookieStore.getAll());
+
   return (
     <main className="container">
       <NewAnime />
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-import { ACCESS_TOKEN_COOKIE, PagesPath, privateRoutes } from '@/utils';
+import { ACCESS_TOKEN_COOKIE, PagesPath } from '@/utils';
 
 export function middleware(req: NextRequest) {
   const isAccessToken = req.cookies.has(ACCESS_TOKEN_COOKIE);
+  console.log(req.cookies.get(ACCESS_TOKEN_COOKIE));
 
   if (!isAccessToken) {
     return NextResponse.redirect(new URL(PagesPath.login, req.url));
